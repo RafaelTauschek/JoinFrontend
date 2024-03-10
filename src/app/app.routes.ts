2d35687860a53beaf2b9b3d80fe2936423c57d15
Rafael Tauschek <rafael.tauschek@gmail.com>
2024-03-10 19:05:21 +0100

    some addings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './components/main/main.component';
-import { AddTaskComponent } from './components/add-task/add-task.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent, children: [
         {path: '', redirectTo: 'login', pathMatch: 'full'},
         {path: 'login', component: LoginComponent},
+        {path: 'register', component: SignUpComponent}
     ]},
-    {path: 'main', component: MainComponent},
+    {path: 'main', component: MainComponent },
 ];
